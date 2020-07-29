76979434732c666c5106f324d09576b9c43fae0f
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-07-30 00:40:52 +1000

    getchFunc
    change to getch function for testing

--- scripts/mobilityScript/mobilityScript.py
+++ scripts/mobilityScript/mobilityScript.py
@@ -2,7 +2,18 @@
 import RPi.GPIO as GPIO
 import numpy as np
 import time
-import getch
+
+
+# Custom Functions
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 # Pin configurations
 PWMA = 5
