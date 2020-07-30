6c82f192e70a80c384149c4e5c8a232b3d82358a
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-07-30 21:00:39 +1000

    added Revolution
    Revolution Feature

--- scripts/mobilityScript/mobilityScript.py
+++ scripts/mobilityScript/mobilityScript.py
@@ -100,7 +100,7 @@ while(1):
         time.sleep(0.2)
 
 
-    elif key == 'd':
+    elif key == 'a':
         GPIO.output(A1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
         GPIO.output(B1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
         GPIO.output(A2,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
@@ -109,7 +109,7 @@ while(1):
         pwmB.ChangeDutyCycle(100)
         time.sleep(0.2)
 
-    elif key == 'a':
+    elif key == 'd':
         GPIO.output(A1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
         GPIO.output(B1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
         GPIO.output(A2,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
@@ -117,6 +117,16 @@ while(1):
         pwmA.ChangeDutyCycle(100)
         pwmB.ChangeDutyCycle(0)
         time.sleep(0.2)
+
+
+    elif key == 'r':
+        GPIO.output(A1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
+        GPIO.output(B1,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
+        GPIO.output(A2,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
+        GPIO.output(B2,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
+        pwmA.ChangeDutyCycle(100)
+        pwmB.ChangeDutyCycle(100)
+        time.sleep(1)
 
 
     elif key == 'q':
