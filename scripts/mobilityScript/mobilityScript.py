7c27109284b4d4572e0139945e7bdc5939065e4c
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-08-30 14:59:32 +1000

    MainFunc+OOP
    Initialised Main function and started working on OOP for Robot

--- scripts/mobilityScript/mobilityScript.py
+++ scripts/mobilityScript/mobilityScript.py
@@ -1,50 +1,24 @@
-# Import Library
+#====================================#
+# EGB320
+# Mobility Script
+# Group SLAVES: Group 13
+# 2020 Semester 2
+# Alan Yu
+#====================================#
+
+
+#---------------#
+# Preamble
+#---------------#
+# Import Python Library for mobility
 import RPi.GPIO as GPIO
 import numpy as np
 import time
 
 
-# Custom Functions
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-getch = _Getch()
-
-
+#---------------#
+# Variables
+#---------------#
 # Pin configurations
 PWMA = 27
 PWMB = 6
@@ -54,20 +28,65 @@ B1 = 11
 B2 = 5
 STBY = 9
 
-GPIO.setmode(GPIO.BCM)				# Set the GPIO pin naming convention
-GPIO.setup(PWMB, GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(PWMA, GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(STBY, GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(A1 , GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(A2 , GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(B1 , GPIO.OUT)			# Set our GPIO pin to output
-GPIO.setup(B2 , GPIO.OUT)			# Set our GPIO pin to output
+# Wheel Parameters
+WHEELRADIUS = 0.03 # Metres
+WHEELBASE = 0.1    # Metres
 
-GPIO.output(STBY,GPIO.HIGH)
-GPIO.output(A1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
-GPIO.output(B1,GPIO.HIGH) 		# Set GPIO pin 21 to digital high (on)
-GPIO.output(A2,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
-GPIO.output(B2,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
+
+
+#---------------#
+# Definition
+#---------------#
+# # Custom Functions
+# class _Getch:
+#     """Gets a single character from standard input.  Does not echo to the
+# screen."""
+#     def __init__(self):
+#         try:
+#             self.impl = _GetchWindows()
+#         except ImportError:
+#             self.impl = _GetchUnix()
+
+#     def __call__(self): return self.impl()
+
+# class _GetchUnix:
+#     def __init__(self):
+#         import tty, sys
+
+#     def __call__(self):
+#         import sys, tty, termios
+#         fd = sys.stdin.fileno()
+#         old_settings = termios.tcgetattr(fd)
+#         try:
+#             tty.setraw(sys.stdin.fileno())
+#             ch = sys.stdin.read(1)
+#         finally:
+#             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#         return ch
+
+# class _GetchWindows:
+#     def __init__(self):
+#         import msvcrt
+
+#     def __call__(self):
+#         import msvcrt
+#         return msvcrt.getch()
+
+# getch = _Getch()
+
+
+
+
+# GPIO.setmode(GPIO.BCM)				# Set the GPIO pin naming convention
+# GPIO.setup(PWMB, GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(PWMA, GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(STBY, GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(A1 , GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(A2 , GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(B1 , GPIO.OUT)			# Set our GPIO pin to output
+# GPIO.setup(B2 , GPIO.OUT)			# Set our GPIO pin to output
+
+
 
 pwmA = GPIO.PWM(PWMA, 100)			# Initiate the PWM signal
 pwmA.start(0)					# Start a PWM signal with duty cycle at 50%
@@ -150,20 +169,7 @@ while(1):
     pwmB.ChangeDutyCycle(0)
 
 
-    
-    # for dc in range (0, 101, 10):
-    #     pwmA.ChangeDutyCycle(dc)
-    #     pwmB.ChangeDutyCycle(dc)
-    #     time.sleep(0.5)
- 
 
-# pwmA.stop()					# Stop the PWM signal
-# pwmB.stop()					# Stop the PWM signal
-
-# GPIO.output(A1,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
-# GPIO.output(B1,GPIO.LOW) 		# Set GPIO pin 21 to digital high (on)
-
-# GPIO.cleanup()
 
 ################################################################
 # Custom Functions
@@ -173,7 +179,13 @@ while(1):
 
 class Mobility:
 
+    
     def __init__(self):
+        """
+        This initialises the mobility class. It will set relevant GPIO Pins on
+        and also enables HBridge for forward/backward control.
+        The function will end when we set the drive to 0.
+        """
         # Set up GPIO Pins
         GPIO.setmode(GPIO.BCM)				# Set the GPIO pin naming convention
         GPIO.setup(PWMB, GPIO.OUT)			# Set our GPIO pin to output
@@ -183,6 +195,43 @@ class Mobility:
         GPIO.setup(A2 , GPIO.OUT)			# Set our GPIO pin to output
         GPIO.setup(B1 , GPIO.OUT)			# Set our GPIO pin to output
         GPIO.setup(B2 , GPIO.OUT)			# Set our GPIO pin to output
+        # Set up GPIO Output 
+        GPIO.output(STBY,GPIO.HIGH)
+        GPIO.output(A1,GPIO.HIGH) 		
+        GPIO.output(B1,GPIO.HIGH) 		
+        GPIO.output(A2,GPIO.LOW) 		
+        GPIO.output(B2,GPIO.LOW) 		
+        # Zero State
+        self.drive(0, 0)
+
+
+    def drive(self, velocity, angVelocity):
+
+        # Convert v and w to motor percentages
+        speed_l, speed_r = self.veloCalcWheels(v, w)
+        #speed_l *= MULTIPLIER_L
+        #speed_r *= MULTIPLIER_R
+        
+        # Apply scaling to right motor
+        speed_r = speed_r * SCALING_M + SCALING_C
+
+        self.drivePower(speed_l, speed_r)
+
+    def veloCalcWheels(self, velocity, angVelocity):
+        
+		# Calculate linear velocity for each wheel
+        veloLeft = v - w * WHEELBASE
+        veloRight = 2 * v - veloLeft
+
+		# Calculate angular velocity for each wheel
+        angVeloLeft = veloLeft / WHEELRADIUS
+        angVeloRight = veloRight / WHEELRADIUS
+
+        # Convert to power value from 0 to 100
+        powerLeft = angVeloLeft * POWER_TO_RADS
+        powerRight = angVeloRight * POWER_TO_RADS
+
+        return powerLeft, powerRight
 
 
     def gpioClean(self):
@@ -215,70 +264,49 @@ class Mobility:
 # class Drive:
 
 #     def __init__(self):
-
 #         # Setup GPIO
 #         GPIO.setmode(GPIO.BCM)
 #         for pin in np.concatenate([MOTOR_PINS.flatten(), [KICKER_PIN], DRIBBLER_PINS]):
 #             GPIO.setup(int(pin), GPIO.OUT)
-
 #         # Setup PWM
 #         self.motorPWM = [[GPIO.PWM(MOTOR_PINS[0,0], 100), GPIO.PWM(MOTOR_PINS[0,1], 100)],
 #                          [GPIO.PWM(MOTOR_PINS[1,0], 100), GPIO.PWM(MOTOR_PINS[1,1], 100)]]
-
 #         # Turn everything off
 #         self.drive(0, 0)
-
 #     def turnMotor(self, pwms, speed):
 #         #print(speed)
 #         # Constrain speed to 100 and -100
 #         if speed > 100:  speed = 100
 #         if speed < -100: speed = -100
-
 #         # Forward
 #         if speed >= 0:
 #             pwms[0].start(speed)
 #             pwms[1].stop()
-        
 #         # Backwards
 #         else:
 #             pwms[0].stop()
 #             pwms[1].start(-speed)
-	
 #     def drive(self, v, w):
-
 #         # Convert v and w to motor percentages
 #         speed_l, speed_r = self.vw2wheels(v, w)
 #         #speed_l *= MULTIPLIER_L
-#         #speed_r *= MULTIPLIER_R
-        
+#         #speed_r *= MULTIPLIER_R        
 #         # Apply scaling to right motor
 #         speed_r = speed_r * SCALING_M + SCALING_C
-
 #         self.drivePower(speed_l, speed_r)
-
 #     def drivePower(self, power_l, power_r):
 #         """ Drive speeds from -100 to 100 """
-
 #         # Turn motors
 #         self.turnMotor(self.motorPWM[0], power_l)
 #         self.turnMotor(self.motorPWM[1], power_r)
-
-#     def vw2wheels(self, v, w):
-        
+#     def vw2wheels(self, v, w):        
 # 		# Calculate linear velocity for each wheel
 #         v_l = v - w * WHEEL_BASE
 #         v_r = 2 * v - v_l
-
 # 		# Calculate angular velocity for each wheel
 #         w_l = v_l / WHEEL_RADIUS
 #         w_r = v_r / WHEEL_RADIUS
-
 #         # Convert to power value from 0 to 100
 #         power_l = w_l * POWER_TO_RADS
 #         power_r = w_r * POWER_TO_RADS
-
 #         return power_l, power_r
-
-
-#     def cleanup(self):
-#         GPIO.cleanup()
---
+++ scripts/slavesMain.py
@@ -0,0 +1,60 @@
+#====================================#
+# EGB320
+# Main Script
+# Group SLAVES: Group 13
+# 2020 Semester 2
+# Alan Yu
+# 
+#====================================#
+
+
+#---------------#
+# Preamble
+#---------------#
+# Import Python Library for main
+import sys, time
+
+
+
+# Set Global Parameters 
+SIMULATION = False
+
+# Local modules
+if SIMULATION:
+    pass
+else:
+    from mobilityScript import mobilityScript
+    # from navigationScript import 
+    # from collectionScript import
+    # from visionScript import
+
+
+#---------------#
+# MainScript
+#---------------#
+
+if __name__ == '__slavesMain__':
+    # Load Objects or Simulation
+    if SIMULATION:
+        pass
+    else:
+        drive = mobilityScript.Mobility()
+
+    # Initialise States
+    vel = 0
+    angVel = 0
+
+
+    # Try Loading And running
+    try:
+        while True:
+            
+            if SIMULATION:
+                sim.SetTargetVelocities(v, 0, w)
+            else:
+                drive.drive(v, w)
+
+    # Clean up when closing        
+    except KeyboardInterrupt:
+        if not SIMULATION:
+            drive.cleanup()
