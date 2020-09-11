6d0e083ea30a9750911ee0e6b10dc75914a92313
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-09-11 10:47:09 +1000

    Update

--- scripts/collectionScript/samp_mgmt.py
+++ scripts/collectionScript/samp_mgmt.py
@@ -1,16 +1,17 @@
+
 import RPi.GPIO as GPIO
 import time
-
 
 servoPin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPin, GPIO.OUT)
 
 #GPIO.output
-pwm = GPIO.PWM(servoPin, 100)
+pwm = GPIO.PWM(servoPin, 50)
+pwm.start(2.5)
 
 def SetAngle(angle):
-    duty = angle / 18 + 2
+    duty = angle / 18 + 1
     GPIO.output(servoPin, True)
     pwm.ChangeDutyCycle(duty)
     time.sleep(1)
@@ -20,12 +21,12 @@ def SetAngle(angle):
 
 def sample_uncovered():  # Start sample_retrival 110mm away
     SetAngle(90)
-    time.sleep(5)
+    time.sleep(2)
     SetAngle(0)
 
 def sample_covered():  # Start sample_retrival 110mm away
     SetAngle(0)
-    time.sleep(5)
+    time.sleep(2)
     SetAngle(90)
 
 while True:
