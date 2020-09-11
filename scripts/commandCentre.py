2805ef2b72db62b370d0c15ac9fad0a747e12374
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-09-11 12:41:16 +1000

    tryDeubugOff

--- scripts/commandCentre.py
+++ scripts/commandCentre.py
@@ -112,13 +112,14 @@ if __name__ == '__main__':
                     pass
                 elif userSelect == "m":
                     drive.manualControl()
-                    print("Starting Command Centre ...")
-                    app.run(host='0.0.0.0',port=6969,debug=True)
                 elif userSelect == "n":
                     drive.continuousControl()
                 elif userSelect == "c":
                     print("Starting Command Centre ...")
                     app.run(host='0.0.0.0',port=6969,debug=True)
+                elif userSelect == "v":
+                    print("Starting Command Centre ...")
+                    app.run(host='0.0.0.0',port=6969,debug=False)
                 elif userSelect == "q":
                     drive.gpioClean()
                     break
