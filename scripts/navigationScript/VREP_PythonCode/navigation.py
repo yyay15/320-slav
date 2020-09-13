94695a5018daa9da4bac1b0a7c721ef864326d92
Vanessa <42192290+vanessa108@users.noreply.github.com>
2020-09-13 14:45:42 +1000

    imprvements but also worse sample retrieval

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
-import math
+from math import * 
+import matplotlib.pyplot as plt
 
 #Mode 
 SEARCH_SAMPLE = 1
@@ -12,20 +13,21 @@ NAV_LANDER = 6
 ACQUIRE_SAMPLE = 7
 DRIVE_UP = 8
 
-CAMERA_BLIND = 0.1
+CAMERA_BLIND = 0.2
 DRIVE_OFF_TIME = 6
 FULL_ROTATION = 32
 
-KV_ATTRACT = 5
-KW_ATTRACT = 2
-KV_REPULSE = 0.1
-KW_REPULSE = 0.2
-
-
+KV_ATTRACT = 0.7
+KW_ATTRACT = 0.8
+KV_REPULSE = 1
+KW_REPULSE = 0.4
 class Navigation:
     def __init__(self):
         self.stateMode = SEARCH_SAMPLE
         self.modeStartTime = time.time()
+        self.prevstate = SEARCH_SAMPLE
+        self.turnDir = 1
+        
     
     def currentState(self, stateNum):
         switchState = {
@@ -57,40 +59,49 @@ class Navigation:
                 v, w = self.driveForward()
             else:
                 v = 0
-                w = 0.2
+                w = 0.2 * self.turnDir
 
 
         if (state.sampleRB != None):
+            self.prevstate = self.stateMode
             self.stateMode = 3
         
         return v, w
 
     def navSample(self, state):
+        print(state.sampleRB)
         if (state.sampleRB == None):
             if (state.prevSampleRB == None):
                 v = 0
                 w = 0
                 print("returing to sample search")
                 self.stateMode = SEARCH_SAMPLE
-            elif(state.prevSampleRB[0][0] < CAMERA_BLIND):
+            elif(state.prevSampleRB[0][0] < CAMERA_BLIND and (state.prevSampleRB[0][1] <0.1 and state.prevSampleRB[0][1] > 0.1)):
                 print ("acquiring sample")
                 v = 0
                 w = 0
+                self.modeStartTime = time.time()
                 self.stateMode = ACQUIRE_SAMPLE
             else:
                 v = 0
                 w = 0
-
+                self.turnDir = np.sign(state.prevSampleRB[0][1])
+                print("returing to sample search")
+                self.stateMode = SEARCH_SAMPLE
         else:
             currSample = state.sampleRB[0]
             #print(currSample)
-            vA, wA = self.attractivePotential(currSample)
-            vR, wR = self.repulsivePotential(state)
-            v = vA - vR
-            w = wA - wR
+            # v, attF = self.attractivePotential(currSample)
+            # repF = self.repulsivePotential(state)
+            # F = (attF + repF)
+            # print(F)
+            # w = F * 5
+            # v = v
+            # v = vA -vR
+            # w = wA - wR
             # v = 0.5* currSample[0]
             # w = currSample[1]
-
+            v, w = self.totalPotential(currSample, state)
         #print("navigating to sample")
         return v, w
 
@@ -136,21 +147,16 @@ class Navigation:
                 w = 0
 
         else:
-            # if (state.landerRB[0] < 0.4):
-            #     v = state.landerRB[0]
-            # else:
-            #     v = 0.5* state.landerRB[0]
-            # w = state.landerRB[1]
-            vA, wA = self.attractivePotential(state.landerRB)
-            vR, wR = self.repulsivePotential(state)
-            v = vA - vR
-            w = wA - wR*10
+            v, w = self.totalPotential(state.landerRB, state)
         return v,w
     def acquireSample(self, state):
         if (not state.sampleCollected):
             v = 0.1
             w = 0
-        
+        elif (time.time() - self.modeStartTime >= 2):
+            v = 0
+            w = 0
+            self.stateMode = SEARCH_SAMPLE
         else:
             v = 0
             w = 0
@@ -185,21 +191,38 @@ class Navigation:
             w = 0
         return v, w
 
+    def totalPotential(self, goal, state):
+        attractive = self.attractivePotential(goal)
+        repulsive = self.repulsivePotential(state)
+        print("r:", repulsive)
+        print("aL:", attractive)
+        totalForce = attractive + repulsive
+        v = max(0.2 * exp(-0.8*repulsive) - 0.05, 0)
+        w = totalForce * 5
+        print(w)
+        return v, w
+
     def attractivePotential(self, goal):
-        v = 0.5 * KV_ATTRACT * goal[0]**2 
-        w = 0.5 * KW_ATTRACT * goal[1]**2  
-        return v, w
+        #v = 0.5 * KV_ATTRACT * goal[0]**2 
+        attractive = 0.5 * KW_ATTRACT * (goal[1])**2 * (exp(-0.4 * goal[0]) + 0.1)
+        return attractive
     def repulsivePotential(self, state):
-        v = 0 
-        w = 0
+        repulsive = 0
+        c3 = 2
+        c4 = 2
         obstacles = state.obstaclesRB
+
         if obstacles != None:
-            for obstacle in obstacles:
-                if obstacle[0] < 0.5:
-                    v = 0.5 * KV_REPULSE * obstacle[0]**2
-                    #w = obstacle[1] * (0.5 - obstacle[0])* (3-abs(obstacle[1])) * KW_REPULSE
-                    #w = 0.5 * KW_REPULSE * obstacle[1]**2
-                    w = KW_REPULSE * obstacle[1]
-            v = 0
-            w = 0
-        return v, w
+            minObstacle = obstacles[0]
+            # for obstacle in obstacles:
+            #     if obstacle[0] < minObstacle[0]:
+            #         minObstacle = obstacle
+            dO = minObstacle[0]
+            hO = minObstacle[1]
+            if dO < 0.5:
+                
+                #repulsive = 0.1* (1/0.01 - 1/(dO))
+                #repulsive =  KW_REPULSE * ((c3 * abs(hO))+1/ c3**2) * exp(-c3*abs(3-hO))* exp(-c4*dO)
+                repulsive =  KW_REPULSE * ((c3 * abs(hO))+1/ c3**2) * exp(-c3*abs(hO))* exp(-c4*dO)
+        
+        return repulsive
