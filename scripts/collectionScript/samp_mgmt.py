411e96185c2adab5b5e481fa2588fc92bd1adfe1
Luis <luis.serrano.4711@gmail.com>
2020-09-15 17:57:48 +1000

    servo working

--- scripts/collectionScript/samp_mgmt.py
+++ scripts/collectionScript/samp_mgmt.py
@@ -1,45 +1,66 @@
 
+
 import RPi.GPIO as GPIO
 import time
 
-
-servoPin = 17
+servoPIN = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPin, GPIO.OUT)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-#GPIO.output
-pwm = GPIO.PWM(servoPin, 50)
-pwm.start(2.5) # Initialise
-
-#def SetAngle(angle):
-  #  duty = angle / 18 + 2
-   # GPIO.output(servoPin, True)
-  #  pwm.ChangeDutyCycle(duty)
- #   GPIO.output(servoPin, False)
- #   pwm.ChangeDutyCycle(0)
+pwm = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+pwm.start(3) # Initialization
 
 
 def sample_uncovered():  # Start sample_retrival 110mm away
-    pwm.ChangeDutyCycle(12.5)
-    time.sleep(5)
-    pwm.ChangeDutyCycle(0)
+    print("Sample Uncovered")
+    pwm.ChangeDutyCycle(7.5)
+    print("Open")
+    time.sleep(1)
+    pwm.ChangeDutyCycle(4)
+    print("Closed")
+    time.sleep(3)
 
 def sample_covered():  # Start sample_retrival 110mm away
-    pwm.ChangeDutyCycle(0)
-    time.sleep(3)
-    pwm.ChangeDutyCycle(12.5)
-    time.sleep(2)
-    pwm.ChangeDutyCycle(0)
+    print("Sampled Covered")
+    pwm.ChangeDutyCycle(4)
+    print("Open")
+    time.sleep(1)
+    pwm.ChangeDutyCycle(7.5)
+    print("Closed")
+    time.sleep(1)
+    pwm.ChangeDutyCycle(4)
+    print("Open")
+    time.sleep(1)
 
-while (1):
-    sample_uncovered()
-    sample_covered()
 
-def sample_release(): 
-    pwm.ChangeDutyCycle(0)
+def sample_release():
+    print("Sample Release")
+    pwm.ChangeDutyCycle(3.5)
+    print("Open")
     time.sleep(2)
-    pwm.ChangeDutyCycle(12.5)
+    pwm.ChangeDutyCycle(7.5)
+    print("Closed")
     time.sleep(6)
-    pwm.ChangeDutyCycle(0)   # 
+    pwm.ChangeDutyCycle(3.5)   
+
+def sample_reset():
+    print("Sample Reset")
+    pwm.ChangeDutyCycle(0.0)
+    print("clean")
+    time.sleep(2)
+    
+try:
+  while True:
+    sample_reset()
+    time.sleep(2)
+    sample_covered()
+    time.sleep(2)
+    sample_reset()
+    time.sleep(5)
+    sample_uncovered()
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
+    print("clean")
     
 
