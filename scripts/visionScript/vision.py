1a7c1d5929d4327ade25d49bb06b4f1a28431069
Vanessa <vanessa.li0810@gmail.com>
2020-09-15 11:19:14 +1000

    cv2 umat

--- scripts/visionScript/vision.py
+++ scripts/visionScript/vision.py
@@ -31,7 +31,7 @@ class Vision:
         #image=cv2.resize(image,(640,480))
         #cv2.imshow("normal",image)
         ogimg=image#store the image given as a parameter for later bitwise and operation
-        image=cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2HSV)
+        image=cv2.cvtColor(cv2.UMAT(image), cv2.COLOR_BGR2HSV)
 
         #image=cv2.GaussianBlur(image, (17, 17), 2) 
         lower=np.array([parameters_dict["hue"][0],parameters_dict["sat"][0],parameters_dict["value"][0]])
