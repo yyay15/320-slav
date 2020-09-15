e7a19fff5392bd57636382deeaf840f666811d78
Vanessa <vanessa.li0810@gmail.com>
2020-09-15 10:20:32 +1000

    vrep config

--- scripts/navigationScript/VREP_PythonCode/main.py
+++ scripts/navigationScript/VREP_PythonCode/main.py
@@ -22,6 +22,24 @@ else:
 if SIMULATION:
     # SET SCENE PARAMETERS
     sceneParameters = rbot.SceneParameters()
+    # SET ROBOT PARAMETERS
+    robotParameters = rbot.RobotParameters()
+    robotParameters.driveType = 'differential'	# specify if using differential (currently differential is only supported)
+    # Drive Parameters
+    robotParameters.minimumLinearSpeed = 0.0  	# minimum speed at which your robot can move forward in m/s
+    robotParameters.maximumLinearSpeed = 0.25 	# maximum speed at which your robot can move forward in m/s
+    robotParameters.driveSystemQuality = 1		# specifies how good your drive system is from 0 to 1 (with 1 being able to drive in a perfectly straight line when a told to do so)
+
+    # Camera Parameters
+    robotParameters.cameraOrientation = 'landscape' # specifies the orientation of the camera, either landscape or portrait
+    robotParameters.cameraDistanceFromRobotCenter = 0.1 # distance between the camera and the center of the robot in the direction of the kicker/dribbler in metres
+    robotParameters.cameraHeightFromFloor = 0.15 # height of the camera relative to the floor in metres
+    robotParameters.cameraTilt = 0.35 # tilt of the camera in radians
+
+    # Vision Processing Parameters
+    robotParameters.maxBallDetectionDistance = 1 # the maximum distance away that you can detect the ball in metres
+    robotParameters.maxLanderDetectionDistance = 2.5 # the maximum distance away that you can detect the goals in metres
+    robotParameters.maxObstacleDetectionDistance = 1.5 # the maximum distance away that you can detect the obstacles in metres
 
 LED_GREEN = 0
 LED_RED = 1
