148ae1f91092cf31c40bd7c6c97482f477e16bb7
Vanessa <vanessa.li0810@gmail.com>
2020-09-16 14:30:06 +1000

    tweaks to vision

--- scripts/visionScript/vision.py
+++ scripts/visionScript/vision.py
@@ -16,8 +16,8 @@ class Vision:
         self.random = 1
         self.changingVariable = 1
         
-        #i2c = busio.I2C(board.SCL, board.SDA)
-        #sensor = adafruit_vcnl4040.VCNL4040(i2c)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_vcnl4040.VCNL4040(i2c)
         self.cap = cv2.VideoCapture(0)  		# Connect to camera 0 (or the only camera)
         self.cap.set(3, 320)                     	# Set the width to 320
         self.cap.set(4, 240)                      	# Set the height to 240
@@ -156,7 +156,7 @@ class Vision:
 
 # 
     def sampleCollected(self):
-         a=sensor.proximity
+        a=self.sensor.proximity
         if a>=13:
             SamplePresent=True
         else:
