8bbb6382e1f93c6d63cbb69ad40af293862dc736
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-09-18 22:17:27 +1000

    revert Changes for push

--- scripts/slavesMain.py
+++ scripts/slavesMain.py
@@ -100,13 +100,17 @@ def collectionControl(command):
 def navVis():
     return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-vc = cv2.VideoCapture(0) 
 def gen():
-   while True: 
-       rval, frame = vc.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
+    camera = cv2.VideoCapture(0)
+    while True:
+        ret, img = camera.read()
+
+        if ret:
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+
 
 #---------------#
 # Initialise
