bfd2aada8083b0eaa963e3a0156de228de9d6fd3
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-09-18 22:23:58 +1000

    remove testing for video feed for now - try another

--- scripts/slavesMain.py
+++ scripts/slavesMain.py
@@ -96,24 +96,6 @@ def collectionControl(command):
     collection.commandCentreCollectionControl(command)
     return '{}'
 
-@app.route('/navVis')
-def navVis():
-    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-vc = cv2.VideoCapture(0)
-def gen():
-    """Video streaming generator function."""
-    while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 #---------------#
