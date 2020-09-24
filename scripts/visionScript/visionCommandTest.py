cf1d53ed410b25a3f922a2cbb0a7f79baac0b9cd
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-09-24 18:40:25 +1000

    revertChange

--- scripts/visionScript/visionCommandTest.py
+++ scripts/visionScript/visionCommandTest.py
@@ -183,30 +183,7 @@ class Vision:
 
     def selfCapRead(self):
         ret, img = self.cap.read()	     		# Get a frame from the camera
-        imgOg = img
-
-        sample_img,SFin=self.Detection(img,self.sample_parameters)
-        cover_img,CFin=self.Detection(img,self.cover_parameters)
-        obstacle_img,OFin=self.Detection(img,self.obstacle_parameters)
-        lander_img,LFin=self.Detection(img,self.lander_parameters)
-        FinalImage=cv2.bitwise_or(SFin,CFin)
-        FinalImage=cv2.bitwise_or(FinalImage,OFin)
-        FinalImage=cv2.bitwise_or(FinalImage,LFin)
-
-
-        print("////////PRINT1/////")
-        print(np.size(imgOg))
-        print("////////PRINT2/////")
-        print(imgOg)
-        print("////////PRINT3/////")
-        print(np.size(FinalImage))
-        print("////////PRINT4/////")
-        FinalImage = cv2.cvtColor(FinalImage, cv2.COLOR_GRAY2RGB)
-        print(FinalImage)
-        print("////////PRINT5/////")
-
-        imgCombi = np.hstack((imgOg,FinalImage))
-        ret, img_str = cv2.imencode('.jpg', imgCombi)        
+        ret, img_str = cv2.imencode('.jpg', img)        
         img_str = img_str.tobytes()
 
         return img_str
