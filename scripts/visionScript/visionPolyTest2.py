7faa9f5cf765423018809053dcd68985ef06359b
SebastianPoh <sebbypro13@gmail.com>
2020-10-01 00:31:30 +1000

    Update Poly2

--- scripts/visionScript/visionPolyTest2.py
+++ scripts/visionScript/visionPolyTest2.py
@@ -118,8 +118,8 @@ def Range(img,parameters_dict,finalimage):
                         Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
                         Distance=((-0.0002*Distance**2)+(0.8492*Distance)+51)/1000
                         ZDistance=np.append(ZDistance,Distance)
-                        if ZDistance==[]:
-                            MaxMinLocations(a,finalimage)
+                        
+                        MaxMinLocations(a,finalimage)
 
                         Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
                         Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
@@ -136,8 +136,8 @@ def Range(img,parameters_dict,finalimage):
                         Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
                         Distance=((-0.0002*Distance**2)+(0.8492*Distance)+51)/1000
                         ZDistance=np.append(ZDistance,Distance)
-                        if ZDistance==[]:
-                            MaxMinLocations(a,finalimage)
+                        
+                        MaxMinLocations(a,finalimage)
                         Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
                         Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
                         #columnwise
