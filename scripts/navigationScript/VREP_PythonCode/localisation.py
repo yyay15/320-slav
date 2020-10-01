11b429985cad4792e0562244d8f0011669a707ea
Vanessa <vanessa.li0810@gmail.com>
2020-10-01 15:02:51 +1000

    map

--- scripts/navigationScript/VREP_PythonCode/localisation.py
+++ scripts/navigationScript/VREP_PythonCode/localisation.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time 
+from math import *
 
 WHEELRADIUS = 0.03 # Metres
 WHEELBASE = 0.13    # Metres
@@ -7,12 +9,30 @@ class Localisation:
     def __init__(self):
         self.wL = 0
         self. wR = 0
-        position = np.zeros((3, 1)) # x, y, theta
-        deltaPos = np.zerps((3, 1)) #dot{ x, y, theta }
+        self.position = np.zeros((3, 1)) # x, y, theta
+        self.deltaPos = np.zerps((3, 1)) #dot{ x, y, theta }
+        self.timer = time.time()
     
     def getWheelAngVel(self, wL, wR, w):
         self.wL = wL
         self.wR = wR
+        v1 = wR * WHEELRADIUS
+        v2 = wL * WHEELRADIUS
+        w1 = v1/(WHEELBASE)
+        w2 = v2/ (WHEELBASE)
+        v = WHEELBASE/2 * (w1 - w2)
+        w = w1 + w2
+        deltaTime = time.time() - self.timer
+        calculateTransform(v, w, deltaTime)
+        self.timer = time.time()
+
         
     
-    def calculateTransform(self):
+    def calculateTransform(self, v, w, deltaTime):
+        theta = w*deltaTime - self.deltaPos[2]
+        state = np.array([v, w])
+        state = np.vstack(state)
+        transform = np.array([[cos(theta), 0], [sin(theta), 0], [0, 1]])
+        self.deltaPos = np.matmul(transform, state)
+        self.pos = self.deltaPos * deltaTime
+        
