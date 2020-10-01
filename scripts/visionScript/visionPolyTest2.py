a8691eab9e4b2963b8f1e60c988113664a2e2fa5
SebastianPoh <sebbypro13@gmail.com>
2020-10-01 14:41:50 +1000

    Update Poly2

--- scripts/visionScript/visionPolyTest2.py
+++ scripts/visionScript/visionPolyTest2.py
@@ -118,9 +118,7 @@ def Range(img,parameters_dict,finalimage):
                         Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
                         Distance=((-0.0002*Distance**2)+(0.8492*Distance)+51)/1000
                         ZDistance=np.append(ZDistance,Distance)
-                        
                         MaxMinLocations(a,finalimage)
-
                         Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
                         Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
                         #columnwise
@@ -158,6 +156,7 @@ def Range(img,parameters_dict,finalimage):
                         cv2.rectangle(finalimage,(Lx-int(LWidth/2),Ly+int(LHeight/2)),(Lx+int(LWidth/2),Ly-int(LHeight/2)),
                         parameters_dict["BBoxColour"],2)
                         Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
+                        Distance=262.22*numpy.log(Distance)-1222.1
                         ZDistance=np.append(ZDistance,Distance)
                         MaxMinLocations(a,finalimage)
                         Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
@@ -173,8 +172,8 @@ def Range(img,parameters_dict,finalimage):
                         cv2.rectangle(finalimage,(Lx-int(LWidth/2),Ly+int(LHeight/2)),(Lx+int(LWidth/2),Ly-int(LHeight/2)),
                         parameters_dict["BBoxColour"],2)
                         Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
+                        Distance=262.22*numpy.log(Distance)-1222.1
                         ZDistance=np.append(ZDistance,Distance)
-                        
                         MaxMinLocations(a,finalimage)
                         Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
                         Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
