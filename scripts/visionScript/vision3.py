d5866222b77f972f537affa2f7cdb184cf164cbd
SebastianPoh <sebbypro13@gmail.com>
2020-10-03 15:08:57 +1000

    Update vision3 thresholds

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -29,7 +29,7 @@ class Vision:
             "Kernel":False,"Circle":False,"BBoxColour":[0,0,255],"type":1}
         self.obstacle_parameters={"hue":[40,70],"sat":[50,255],"value":[40,255],"Height":80,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[204,204,0],"type":2}
-        self.cover_parameters={"hue":[105,120],"sat":[70,255],"value":[70,255],"Height":70,"OR_MASK":False,
+        self.cover_parameters={"hue":[105,120],"sat":[120,255],"value":[70,255],"Height":70,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[255,255,255],"type":3} 
         self.hole_parameters={"hue":[0,255],"sat":[0,100],"value":[0,80],"Height":50,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[180,0,180],"type":4} 
