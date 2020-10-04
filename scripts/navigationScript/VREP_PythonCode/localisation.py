002f982c346b2d505c495ebf5d71cb493e272e81
Vanessa <vanessa.li0810@gmail.com>
2020-10-04 12:19:01 +1000

    change state

--- scripts/navigationScript/VREP_PythonCode/localisation.py
+++ scripts/navigationScript/VREP_PythonCode/localisation.py
@@ -12,18 +12,12 @@ class Localisation:
         self.position = np.zeros((3, 1)) # x, y, theta
         self.deltaPos = np.zeros((3, 1)) #dot{ x, y, theta }
         self.timer = time.time()
+        self.sampleGlobal = None
+        self.rockGlobal = None 
+        self.state = None
     
-    def getWheelAngVel(self, v, w):
-        # self.wL = wL
-        # self.wR = wR
-        # v1 = wR * WHEELRADIUS
-        # v2 = wL * WHEELRADIUS
-        # w1 = v1/(WHEELBASE)
-        # w2 = v2/ (WHEELBASE)
-        # v = WHEELBASE/2 * (w1 - w2)
-        # print("Velc", v)
-        # w = w1 + w2
-        # w = 0.15
+    def getWheelAngVel(self, v, w, state):
+        self.state = state
         deltaTime = time.time() - self.timer
         self.calculateTransform(v, w, deltaTime)
         self.timer = time.time()
@@ -37,3 +31,22 @@ class Localisation:
         transform = np.array([[cos(theta), 0], [sin(theta), 0], [0, 1]])
         self.deltaPos = np.matmul(transform, state)
         self.position = self.position + self.deltaPos * deltaTime
+
+
+
+    def objectToGlobal(self, objects):
+        objectXY = []
+        for item in objects:
+            objectX = item[0] * cos(item[1])
+            objectY = item[0] * sin(item[1])
+            objectX = objectX + self.position[0]
+            objectY = objectY + self.position[1]
+            objectXY.append([objectX, objectY])
+        return objectXY
+    
+    # def updateExisiting(self, objectXY):
+    #     for coord in objectXY
+        
+
+
+
