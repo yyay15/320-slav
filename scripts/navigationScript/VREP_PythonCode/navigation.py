49bdbe107a474fcd9f207a71178cbc5f1a1c5507
Vanessa <vanessa.li0810@gmail.com>
2020-10-06 23:35:53 +1000

    bad

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -211,17 +211,14 @@ class Navigation:
 
 
     def avoidObstacles(self, state):
-        allObstacles = []
         obstacles = state.obstaclesRB
         rocks = state.rocksRB
         vRep = 0
         wRep = 0
         if not self.isEmpty(obstacles):
-            allObstacles = allObstacles + obstacles
-        if not self.isEmpty(rocks) and self.rock_obstacle:
-            allObstacles = allObstacles + rocks
-        if not self.isEmpty(allObstacles):
-            closeObs = self.closestObstacle(allObstacles)
+            if not self.isEmpty(rocks) and self.rock_obstacle:
+                obstacles = obstacles + rocks
+            closeObs = self.closestObstacle(obstacles)
             if closeObs[0] < 0.5:
                 wRep =  (np.sign(closeObs[1]) * (0.5 - closeObs[0]) * (3 - abs(closeObs[1]))* KW_REPULSE)
                 vRep =  (0.5 - closeObs[0]) * 0.2
