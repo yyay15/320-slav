f80aedfda4dc1824075aa1e88d1e3ece925c5468
SebastianPoh <sebbypro13@gmail.com>
2020-10-06 17:32:27 +1000

    restored vision3.py

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -102,12 +102,7 @@ class Vision:
         #LHeight=np.array([])
         #GrayFiltimg=cv2.cvtColor(img,cv2.COLOR_HSV2BGR)
         #GrayFiltimg=cv2.cvtColor(GrayFiltimg,cv2.COLOR_RGB2GRAY)
-        if parameters_dict["type"]==3:
-            justCovers=cv2.bitwise_or(finalimage,img)
-            edges=cv2.Canny(justCovers,100,200)
-            Contour=cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-        else:
-            Contour=cv2.findContours(img,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+        Contour=cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         if Contour == []:
             print("there is nothing here")
         else:
@@ -128,7 +123,6 @@ class Vision:
                             Distance=Distance/1000
                             ZDistance=np.append(ZDistance,Distance)
                             Bearing=np.append(Bearing,math.radians((x-160)*(31.1/160)))
-                            cv2.drawContours(finalimage,a,-1,(0,255,0),3)
                             #print range bearing on image
                             textOrigin = (int(x-radius),int(y-radius)+ 5)
                             rangeText = "R: {:.4f}".format(Distance)
@@ -143,7 +137,6 @@ class Vision:
                     else:
                         continue 
                 elif parameters_dict["type"]==3:
-                    
                     Lx1,Ly1,LWidth,LHeight=cv2.boundingRect(a)
                     if (LWidth*LHeight)>300:
                         #if (LWidth/LHeight)<=2 and (LHeight/LWidth)<=2:
@@ -155,7 +148,6 @@ class Vision:
                         parameters_dict["BBoxColour"],2)
                         Distance=(parameters_dict["Height"]*(self.f/LHeight)/8)*math.cos(0.2967)
                         Distance=((-0.0002*Distance**2)+(0.8492*Distance)+51)/1000
-                        cv2.drawContours(finalimage,a,-1,(0,255,0),3)
                         ZDistance=np.append(ZDistance,Distance)
                         New_Lx,left_diff,right_diff=self.MaxMinLocations(a,finalimage,Lx)
                         NewBearing=np.append(NewBearing,math.radians((New_Lx-160)*(31.1/160)))
@@ -186,7 +178,6 @@ class Vision:
                         Center=np.append(Center,Centroid)
                         cv2.rectangle(finalimage,(Lx-int(LWidth/2),Ly+int(LHeight/2)),(Lx+int(LWidth/2),Ly-int(LHeight/2)),
                         parameters_dict["BBoxColour"],2)
-                        cv2.drawContours(finalimage,a,-1,(0,255,0),3)
                         Distance=(parameters_dict["Height"]*(self.f/LHeight)/8)*math.cos(0.2967)
                         Distance=(262.22*np.log(Distance)-1222.1)/1000
                         ZDistance=np.append(ZDistance,Distance)
@@ -217,7 +208,7 @@ class Vision:
                             Distance=(parameters_dict["Height"]*(self.f/LHeight)/8)*math.cos(0.2967)
                             Distance=(0.8667*Distance-3)/1000
                             ZDistance=np.append(ZDistance,Distance)
-                            cv2.drawContours(finalimage,a,-1,(0,255,0),3)
+                            
                             Bearing=np.append(Bearing,math.radians((Lx-160)*(31.1/160)))
                             #print range bearing
                             textOrigin = (Lx-int(LWidth/2),Ly-int(LHeight/2)+ 5)
