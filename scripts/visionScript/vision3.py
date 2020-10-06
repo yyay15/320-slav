d44fa5feb5a8995f2d818ee067cc121b2a910844
SebastianPoh <sebbypro13@gmail.com>
2020-10-06 12:13:53 +1000

    update vision3

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -47,8 +47,8 @@ class Vision:
         maxright=tuple(c[c[:,:,0].argmax()][0])
         maxtop=tuple(c[c[:,:,1].argmin()][0])
         maxbot=tuple(c[c[:,:,1].argmax()][0])
-        right_diff=maxright[2]-maxbot[2]
-        left_diff=maxleft[2]-maxleft[2]
+        right_diff=maxright[0]-maxbot[0]
+        left_diff=maxleft[0]-maxleft[0]
         cv2.line(img,maxbot,maxright,(0,255,0),2)
         cv2.line(img,maxbot,maxleft,(0,255,0),2)
         if left_diff>5 and right_diff>5:
