a5a147ea8df5f3aa6c50722fcbb44847b9974283
SebastianPoh <sebbypro13@gmail.com>
2020-10-06 23:41:07 +1000

    update vision3 to fit for lander hole

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -31,8 +31,8 @@ class Vision:
             "Kernel":False,"Circle":False,"BBoxColour":[204,204,0],"type":2}
         self.cover_parameters={"hue":[95,107],"sat":[100,255],"value":[0,200],"Height":70,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[255,255,255],"type":3} 
-        self.hole_parameters={"hue":[0,255],"sat":[0,255],"value":[50,60],"Height":50,"OR_MASK":False,
-            "Kernel":False,"Circle":False,"BBoxColour":[255,0,0],"type":4} 
+        self.hole_parameters={"hue":[0,255],"sat":[95,255],"value":[20,40],"Height":50,"OR_MASK":False,
+            "Kernel":False,"Circle":False,"BBoxColour":[180,0,180],"type":4} 
         self.wall_parameters={"hue":[0,255],"sat":[0,255],"value":[0,30],"Height":80,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[255,0,0],"type":6} 
         self.coverhole_parameters={"hue":[0,255],"sat":[0,255],"value":[0,50],"Height":10,"OR_MASK":False,
@@ -222,28 +222,25 @@ class Vision:
                             Range=Range[Range[:,0].argsort()] 
                         else:
                             continue
-                elif parameters_dict["type"]==4: #for hole on lander
-                    Lx1,Ly1,LWidth,LHeight=cv2.boundingRect(a)
+                elif parameters_dict["type"]==4:
+                Lx1,Ly1,LWidth,LHeight=cv2.boundingRect(a)
+                if Area>30 and Area<2000:
+                    #if LWidth/LHeight<1.1 and LHeight/LWidth<1.1:
                     (x,y),radius=cv2.minEnclosingCircle(a)
-                    if Area>10 and Area<6000:
-                        cv2.rectangle(finalimage,(int(x-radius),int(y+radius)),(int(x+radius),int(y-radius)),
-                        parameters_dict["BBoxColour"],2)
-                        Distance=(parameters_dict["Height"]*(self.f/(2*radius))/8)*math.cos(0.2967)
-                        Distance=(-0.0005*Distance**2)+(1.4897*Distance)-66.919
-                        Distance=Distance/1000
-                        ZDistance=np.append(ZDistance,Distance)
-                        Bearing=np.append(Bearing,math.radians((x-160)*(31.1/160)))
-                        #print range bearing on image
-                        textOrigin = (int(x-radius),int(y-radius)+ 5)
-                        rangeText = "R: {:.4f}".format(Distance)
-                        bearingText = " B: {:.4f}".format((math.radians((x-160)*(31.1/160))))
-                        cv2.putText(finalimage, rangeText + bearingText, textOrigin, cv2.FONT_HERSHEY_SIMPLEX, 
-                         0.4,  parameters_dict["BBoxColour"] )
-                        Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
-                        #columnwise
-                        Range=Range[Range[:,0].argsort()]
-                    else:
-                        continue 
+                    cv2.rectangle(finalimage,(int(x-radius),int(y+radius)),(int(x+radius),int(y-radius)),
+                    parameters_dict["BBoxColour"],2)
+                    Distance=(parameters_dict["Height"]*(self.f/(2*radius))/8)*math.cos(0.2967)
+                    Distance=(-0.0005*Distance**2)+(1.4897*Distance)-66.919
+                    Distance=Distance/1000
+                    ZDistance=np.append(ZDistance,Distance)
+                    Bearing=np.append(Bearing,math.radians((x-160)*(31.1/160)))
+                    Range=np.vstack((ZDistance,-Bearing)).T#Put Bearing and ZDistance into one array and arrange
+                    #columnwise
+                    Range=Range[Range[:,0].argsort()]
+                    #else:
+                    #    continue
+                else:
+                    continue  
                 elif parameters_dict["type"]==5:
                     Lx1,Ly1,LWidth,LHeight=cv2.boundingRect(a)
                     if Area>20 and Area<2000:
@@ -334,7 +331,7 @@ class Vision:
             self.lander_parameters.update(Lander_parameter_update)#update dictionary for lander
             #to change values to adjust for dodge lighting when going up lander
             inverted_Lander=cv2.bitwise_not(LanderImage)
-            lander_hole,abc=self.Range(inverted_Lander,self.coverhole_parameters,finalImage)
+            lander_hole=self.Range(inverted_Lander,self.hole_parameters,finalImage)
             #hole_img=self.Detection(img,self.hole_parameters)
             #hole_Z=self.Range(hole_img,self.hole_parameters,img)
             #LanderMasklow=np.array([15,0,0],dtype="uint8")
