d537cec0c3834653c80923d305acb1ce408c1dc0
SebastianPoh <sebbypro13@gmail.com>
2020-10-06 20:26:09 +1000

    update vision3

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -253,7 +253,7 @@ class Vision:
                         Center=np.append(Center,Centroid)
                         cv2.rectangle(finalimage,(Lx-int(LWidth/2),Ly+int(LHeight/2)),(Lx+int(LWidth/2),Ly-int(LHeight/2)),
                         parameters_dict["BBoxColour"],2)
-                        Distance=(parameters_dict["Height"]*(f/LHeight)/8)*math.cos(0.2967)
+                        Distance=(parameters_dict["Height"]*(self.f/LHeight)/8)*math.cos(0.2967)
                         Distance=0.8667*Distance-3
                         ZDistance=np.append(ZDistance,Distance)
                         #self.MaxMinLocations(a,finalimage)
