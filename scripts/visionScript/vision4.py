540689883a2dc63ea1e72bcc0c89cfbaf7d0c3f4
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-07 15:43:39 +1000

    Update vision4.py

--- scripts/visionScript/vision4.py
+++ scripts/visionScript/vision4.py
@@ -51,17 +51,17 @@ class Vision:
         maxright=tuple(c[c[:,:,0].argmax()][0])
         maxtop=tuple(c[c[:,:,1].argmin()][0])
         maxbot=tuple(c[c[:,:,1].argmax()][0])
-        #left_diff=maxright[0]-maxbot[0]
-        #right_diff=maxleft[0]-maxbot[0]
-        #right_diff=-right_diff
-        #cv2.line(img,maxbot,maxright,(0,255,0),2)
-        #cv2.line(img,maxbot,maxleft,(0,255,0),2)
-        #if left_diff>15 and right_diff>15:
-        #    if right_diff>left_diff:
-        #        diff=-right_diff
-        #    elif right_diff<left_diff:
-        #        diff=left_diff
-        #    total_diff=Lx+diff #so total diff will be less than Lx if left difference is greater than 
+        left_diff=maxright[0]-maxbot[0]
+        right_diff=maxleft[0]-maxbot[0]
+        right_diff=-right_diff
+        cv2.line(img,maxbot,maxright,(0,255,0),2)
+        cv2.line(img,maxbot,maxleft,(0,255,0),2)
+        if left_diff>15 and right_diff>15:
+            if right_diff>left_diff:
+                diff=-right_diff
+            elif right_diff<left_diff:
+                diff=left_diff
+            total_diff=Lx+diff #so total diff will be less than Lx if left difference is greater than 
             #otherwise then we have to angle left if total diff is greater than Lx that 
             # means that we have to angle right:
         else:
