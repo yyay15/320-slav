30c23d7537d15de6c3bc52f00e99e0525c05fddb
SebastianPoh <sebbypro13@gmail.com>
2020-10-09 03:47:54 +1000

    update vision3

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -226,7 +226,7 @@ class Vision:
                         continue
                 elif parameters_dict["type"]==1: #for lander
                     Lx1,Ly1,LWidth,LHeight=cv2.boundingRect(a)
-                    if Area>2000 and Area<60000:
+                    if Area>10000 and Area<60000:
                         self.landerArea = Area
                         Lx=int(Moment["m10"]/Moment["m00"])
                         Ly=int(Moment["m01"]/Moment["m00"])
