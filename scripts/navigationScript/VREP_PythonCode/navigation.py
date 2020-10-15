107a1dd05b808c924ee88cbbd0c70f9b59a9fec5
Vanessa <vanessa.li0810@gmail.com>
2020-10-15 12:52:05 +1000

    tweak spin

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -611,7 +611,7 @@ class Navigation:
                 if closeObs[0] < 0.5:
                     wTemp =  (np.sign(closeObs[1]) * (0.5 - closeObs[0]) * (3 - abs(closeObs[1]))* KW_REPULSE)
                     vRep =  (0.5 - closeObs[0]) * 0.2
-                if closeObs[0] < 0.2:
+                if closeObs[0] < 0.15:
                     wTemp = 2 * wTemp
                 wRep += wTemp
         return vRep, wRep
