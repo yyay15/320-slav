8cf05124955b70a22f16069a39ce70f2ef370136
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-15 17:41:56 +1000

    subhfu9sgfisugfs

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -266,11 +266,11 @@ class Navigation:
                 else:
                     # search sample
                     self.modeStartTime = time.time()
-                    self.stateMode = SEARCH_SAMPLE
-                    #if (not self.isEmpty(state.sampleRB)):
-                    #    self.stateMode = SEARCH_SAMPLE
-                    #else:
-                    #    self.stateMode = SEARCH_ROCK
+                    #self.stateMode = SEARCH_SAMPLE
+                    if (not self.isEmpty(state.sampleRB)):
+                        self.stateMode = SEARCH_SAMPLE
+                    else:
+                        self.stateMode = SEARCH_ROCK
         return v, w
 
     def navLander(self, state):
