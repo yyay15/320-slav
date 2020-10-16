8c95599d2bcc72d74138694e18c2f93d38d7a9b0
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-16 15:28:23 +1000

    sdansdiu

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -331,7 +331,7 @@ class Navigation:
             self.isBlind = True
             self.modeStartTime = time.time()
         elif (self.isBlind):
-            if (time.time() - self.modeStartTime < 1.5):
+            if (time.time() - self.modeStartTime < 1.6):
                 print("trying to drive straight YEEEEETTTT")
                 v = 0.07
                 w = 0
