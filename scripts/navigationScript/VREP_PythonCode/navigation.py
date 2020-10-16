ff38e8f8b7c2d3e763b5fe84bcb316be23672c23
Vanessa <vanessa.li0810@gmail.com>
2020-10-16 10:25:55 +1000

    tweak reverse time

--- scripts/navigationScript/VREP_PythonCode/navigation.py
+++ scripts/navigationScript/VREP_PythonCode/navigation.py
@@ -247,7 +247,7 @@ class Navigation:
                 self.flipRockStart= time.time()
                 self.isBlind = True
             elif (self.isBlind):
-                if (time.time() - self.flipRockStart < 0.7):
+                if (time.time() - self.flipRockStart <3):
                     print("cover open, reversing")
                     v = -0.07
                     w = 0
