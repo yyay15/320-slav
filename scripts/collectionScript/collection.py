baffcd5651ef9f43d350a39564b7ad261f9daeb0
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-16 15:46:13 +1000

    0.4

--- scripts/collectionScript/collection.py
+++ scripts/collectionScript/collection.py
@@ -130,12 +130,12 @@ class Collection:
         timeElapsed = time.time() - self.stateTime
         print("I'm Close Rot")
         print(timeElapsed)
-        if timeElapsed < 0.5:
+        if timeElapsed < 0.4:
             if self.count <1:
                 self.servoPWM.ChangeDutyCycle(2.9)
                 self.count+=1
                 print("Close")
-        elif 0.5 < timeElapsed < 1:
+        elif 0.4 < timeElapsed < 1:
             if self.count <2:
                 self.servoPWM.ChangeDutyCycle(0)
                 self.count+=1
