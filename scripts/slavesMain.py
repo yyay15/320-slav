713111fa2f0104d1ba3c1cc3db734a139efb69bd
yyay15 <56522754+yyay15@users.noreply.github.com>
2020-10-17 21:14:46 +1000

    testLogger

--- scripts/slavesMain.py
+++ scripts/slavesMain.py
@@ -66,6 +66,18 @@ def ledSetup():
     GPIO.setup(LED_RED, GPIO.OUT) 
 
 
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+
+f = open('logfile', 'w')
+backup = sys.stdout
+sys.stdout = Tee(sys.stdout, f)
+
+
 
 #---------------#
 # Flask Function
