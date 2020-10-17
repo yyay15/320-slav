2cc0195119017522eda696a046d8911754c6923b
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-17 13:16:19 +1000

    libuj

--- scripts/slavesMain.py
+++ scripts/slavesMain.py
@@ -217,21 +217,8 @@ if __name__ == '__main__':
                     time.sleep(2)
                     collection.Close_ROT()
                     time.sleep(1)
-                    collection.Lander()
-                    time.sleep(1)
-                    drive.drive(0.08,0, False)
-                    time.sleep(1.5)
-                    drive.drive(0,0, False)
-                    break
-                    drive.drive(0.07, 0, False)
-                    time.sleep(1)
-                    drive.drive(0, 0, False)
-                    collection.Close_ROT()
-                    drive.drive(0.1, 0, False)
-                    time.sleep(1)
-                    drive.drive(0, 0, False)
-
-                app.run(host='0.0.0.0',port=6969,debug=False)
+    
+                    app.run(host='0.0.0.0',port=6969,debug=False)
             elif userSelect == "q":
                 drive.gpioClean()
                 break
