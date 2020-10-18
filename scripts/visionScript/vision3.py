ee11d8f4cdf7bcc50e2823fde512c02156f1c7ce
luisserrano504 <61722574+luisserrano504@users.noreply.github.com>
2020-10-18 18:42:14 +1000

    update vision3

--- scripts/visionScript/vision3.py
+++ scripts/visionScript/vision3.py
@@ -30,7 +30,7 @@ class Vision:
             "Kernel":True,"Circle":True,"BBoxColour":[204,0,204],"type":0}
         self.lander_parameters={"hue":[15,30],"sat":[70,255],"value":[100,255],"Height":80,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[0,0,255],"type":1}
-        self.obstacle_parameters={"hue":[40,70],"sat":[50,255],"value":[40,255],"Height":80,"OR_MASK":False,
+        self.obstacle_parameters={"hue":[40,70],"sat":[30,255],"value":[20,255],"Height":80,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[204,204,0],"type":2}
         self.cover_parameters={"hue":[95,107],"sat":[130,255],"value":[30,200],"Height":70,"OR_MASK":False,
             "Kernel":False,"Circle":False,"BBoxColour":[255,255,255],"type":3} 
